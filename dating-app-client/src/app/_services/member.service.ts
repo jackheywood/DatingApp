57a224481efd0cc47bd301d9cc24c80659d566e1
Jack Heywood <jackaheywood@gmail.com>
2024-02-20 21:33:06 +0000

    Add JWT interceptor to add bearer token to API requests

--- dating-app-client/src/app/_services/member.service.ts
+++ dating-app-client/src/app/_services/member.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Member } from '../_models/member';
 import { Observable } from 'rxjs';
 
@@ -13,21 +13,10 @@ export class MemberService {
   constructor(private http: HttpClient) {}
 
   getMembers(): Observable<Member[]> {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string): Observable<Member> {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token,
-      }),
-    };
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 }
