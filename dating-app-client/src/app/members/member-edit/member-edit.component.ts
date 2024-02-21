db4eb87044ea228e791b98d296a2ed2a2fb879d5
Jack Heywood <jackaheywood@gmail.com>
2024-02-21 21:10:11 +0000

    Add dummy edit member form

--- dating-app-client/src/app/members/member-edit/member-edit.component.ts
+++ dating-app-client/src/app/members/member-edit/member-edit.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { take } from 'rxjs';
 
 import { Member } from '../../_models/member';
 import { User } from '../../_models/user';
 import { AccountService } from '../../_services/account.service';
 import { MemberService } from '../../_services/member.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -12,20 +14,36 @@ import { MemberService } from '../../_services/member.service';
   styleUrls: ['./member-edit.component.scss'],
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm | undefined;
   member: Member | undefined;
   user: User | null = null;
 
-  constructor(private accountService: AccountService, private memberService: MemberService) {
+  constructor(
+    private accountService: AccountService,
+    private memberService: MemberService,
+    private toastr: ToastrService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: (user) => this.user = user,
     });
+  }
+
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm?.dirty) {
+      $event.returnValue = true;
+    }
   }
 
   ngOnInit(): void {
     this.loadMember();
   }
 
-  loadMember() {
+  updateMember(): void {
+    console.log(this.member);
+    this.toastr.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
+  }
+
+  private loadMember(): void {
     if (!this.user) return;
     this.memberService.getMember(this.user.username).subscribe({
       next: member => this.member = member,
